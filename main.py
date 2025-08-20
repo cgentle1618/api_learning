eda47051798fceba24d39be09eaa0fe7c9f28b7c
cgentle1618 <cgentle1618@gmail.com>
2025-08-20 10:02:14 +0800

    first commit with content

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi import Body
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+
+@app.post("/posts")
+def create_posts(new_post: Post):
+    print(new_post.rating)
+    print(new_post.dict())
+    return {"data": new_post}
