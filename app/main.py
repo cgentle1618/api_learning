89767d854a09943dbe903b1ae5746a694276aebd
cgentle1618 <cgentle1618@gmail.com>
2025-08-23 09:32:43 +0800

    Cleaning codes

--- app/main.py
+++ app/main.py
@@ -73,13 +73,6 @@ def test_posts(db: Session = Depends(get_db)):
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post, db: Session = Depends(get_db)):
-    # cursor.execute(
-    #     "INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *",
-    #     (post.title, post.content, post.published),
-    # )
-    # new_post = cursor.fetchone()
-
-    # conn.commit()
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -89,16 +82,12 @@ def create_posts(post: Post, db: Session = Depends(get_db)):
 
 @app.get("/posts")
 def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("SELECT * FROM posts")
-    # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
     return {"data": posts}
 
 
 @app.get("/posts/{id}")
 def get_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("SELECT * FROM posts WHERE id = %s", (id,))
-    # post = cursor.fetchone()
     post = db.query(models.Post).filter(models.Post.id == id).first()
 
     if not post:
@@ -111,17 +100,6 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 @app.put("/posts/{id}")
 def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
-    # cursor.execute(
-    #     "UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *",
-    #     (
-    #         post.title,
-    #         post.content,
-    #         post.published,
-    #         id,
-    #     ),
-    # )
-    # updated_post = cursor.fetchone()
-    # conn.commit()
 
     post_query = db.query(models.Post).filter(models.Post.id == id)
 
@@ -141,9 +119,6 @@ def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("DELETE FROM posts WHERE id = %s RETURNING *", (id,))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
 
     post = db.query(models.Post).filter(models.Post.id == id)
 
